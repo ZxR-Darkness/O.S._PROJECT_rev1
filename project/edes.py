ac8505e368e613b38d48e20133903a3a9cbc5a3d
darkneAss <imperium.foxes1@gmail.com>
2023-09-22 19:01:38 +0300

    editing2

--- project/edes.py
+++ project/edes.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 def filesave():
-    secrets2 = open('DSD.txt','w')  # открытие в режиме записи  # не используеться в данный момент
+    secrets2 = open('DSD.txt','w')  # открытие в режиме записи  # 
     secrets2.write("его нету и не будет никогда god")
     defing = tk.Tk()
 
@@ -9,6 +9,6 @@ def filesave():
     btn.place_configure(x=23,y=23)
 
 def textone():
-    secrets3 = open('DSD.txt','w')  # открытие в режиме записи  # не используеться в данный момент
+    secrets3 = open('DSD.txt','w')  # открытие в режиме записи  # 
     secrets3.write("слушай а ты хитрый ладно дам тебе один прикол god дамн")
 
