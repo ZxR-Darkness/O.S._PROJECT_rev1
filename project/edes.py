7b83daaae8e1b0e0b66b42d1d8abc4376bd23457
DARKNESS <144697263+darkneASS@users.noreply.github.com>
2024-03-01 21:25:38 +0300

    alpha 0.4.6

--- project/edes.py
+++ project/edes.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 def filesave():
     secrets2 = open('DSD.txt','w')  # открытие в режиме записи   
-    secrets2.write("его нету и не будет никогда god")
+    secrets2.write("его нету и не будет никогда.... ладно слушай скажу даже бог не знает что в этой проге ты можешь продолжить но я бы советовал уйти \n но если хочешь продолжить то вот (GIDS)")
     defing = tk.Tk()
 
     btn = tk.Button(defing, text="create file", command=filesave )
@@ -12,7 +12,7 @@ def filesave():
 
 def textone():
     secrets3 = open('DSD2.txt','w')  # открытие в режиме записи  
-    secrets3.write("слушай а ты хитрый ладно дам тебе один прикол god дамн")
+    secrets3.write("???: ты не видел куда босс клал ключ от ПК? \n ???: да всмысле ты не видел?! \n ???: ладно пофиг кароче иди ты нахуй я сам найду ;№*%?;*(%)")
 
 def texttwo():
     secrets3 = open('Dsd3.txt','w')  # открытие в режиме записи  
